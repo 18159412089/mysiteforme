95e44d1f260a1b88f564a49c4fa558a5a943d04d
18159412089 <786191569@qq.com>
2020-08-13 08:58:25 +0800

    增加启动成功标志

--- src/main/java/com/mysiteforme/admin/MysiteformeApplication.java
+++ src/main/java/com/mysiteforme/admin/MysiteformeApplication.java
@@ -1,20 +1,27 @@
 package com.mysiteforme.admin;
 
+import com.xiaoleilu.hutool.log.Log;
+import com.xiaoleilu.hutool.log.LogFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @MapperScan("com.mysiteforme.admin.dao")
 public class MysiteformeApplication extends SpringBootServletInitializer {
 
-//	@Override
+    //	@Override
 //	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //		return application.sources(MysiteformeApplication.class);
 //	}
+    private static final Log log = LogFactory.get();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         SpringApplication.run(MysiteformeApplication.class, args);
+        log.info("启动成功：" + "http://" + InetAddress.getLocalHost().getHostAddress() + ":8080/");
     }
 }
