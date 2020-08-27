36d43b0274486aada214782dff34b01a5d76017f
18159412089 <786191569@qq.com>
2020-08-27 10:21:01 +0800

    启动地址加端口提示

--- src/main/java/com/mysiteforme/admin/MysiteformeApplication.java
+++ src/main/java/com/mysiteforme/admin/MysiteformeApplication.java
@@ -2,12 +2,16 @@ package com.mysiteforme.admin;
 
 import com.xiaoleilu.hutool.log.Log;
 import com.xiaoleilu.hutool.log.LogFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
@@ -22,18 +26,20 @@ public class MysiteformeApplication extends SpringBootServletInitializer {
 //		return application.sources(MysiteformeApplication.class);
 //	}
     private static final Log log = LogFactory.get();
-
+    private static String url;
     public static void main(String[] args) throws UnknownHostException {
-        SpringApplication.run(MysiteformeApplication.class, args);
-//        log.info("启动成功：" + "http://" + InetAddress.getLocalHost().getHostAddress() + ":8080/");
+        ConfigurableApplicationContext context = SpringApplication.run(MysiteformeApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+        url = "http://" + InetAddress.getLocalHost().getHostAddress() + ":" + environment.getProperty("server.port");
+        log.info("启动成功，访问链接：" + url);
     }
 
-   /* @Component
+    /*@Component
     public class RunHomePage implements CommandLineRunner {
         //启动成功后自动弹出页面
         public void run(String... args) {
             try {
-                Runtime.getRuntime().exec("cmd   /c   start   http://localhost:8080/");
+                Runtime.getRuntime().exec("cmd   /c   start   http://localhost:8081/");
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
